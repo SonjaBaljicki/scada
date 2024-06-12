ae197f288188603dcaf37404c0fc3da8b2e94741
milamilovic <milamilovic03@gmail.com>
2024-06-12 21:25:12 +0200

    login and registration work!!!

---
+++ ScadaCore/ScadaCore/DatabaseManagerService.svc.cs
@@ -0,0 +1,117 @@
+﻿using ScadaCore.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Cryptography;
+using System.ServiceModel;
+using System.Text;
+
+namespace ScadaCore
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "DatabaseManagerService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select DatabaseManagerService.svc or DatabaseManagerService.svc.cs at the Solution Explorer and start debugging.
+    public class DatabaseManagerService : IDatabaseManagerService
+    {
+
+        private static Dictionary<string, User> authenticatedUsers = new Dictionary<string, User>();
+
+        public bool Registration(string username, string password)
+        {
+            string encryptedPassword = EncryptData(password);
+            User user = new User(username, encryptedPassword);
+            using (var db = new DatabaseContext())
+            {
+                try
+                {
+                    db.users.Add(user);
+                    db.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public string Login(string username, string password)
+        {
+            using (var db = new DatabaseContext())
+            {
+                foreach (var user in db.users)
+                {
+                    if (username == user.Username &&
+                   ValidateEncryptedData(password, user.EncryptedPassword))
+                    {
+                        string token = GenerateToken(username);
+                        authenticatedUsers.Add(token, user);
+                        return token;
+                    }
+                }
+            }
+            return "Login failed";
+        }
+
+        public bool LogOut(string token)
+        {
+            return authenticatedUsers.Remove(token);
+        }
+
+        string GenerateToken(string username)
+        {
+            RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider();
+            byte[] randVal = new byte[32];
+            crypto.GetBytes(randVal);
+            string randStr = Convert.ToBase64String(randVal);
+            return username + randStr;
+        }
+
+        bool ValidateEncryptedData(string valueToValidate, string valueFromDatabase)
+        {
+            string[] arrValues = valueFromDatabase.Split(':');
+            string encryptedDbValue = arrValues[0];
+            string salt = arrValues[1];
+            byte[] saltedValue = Encoding.UTF8.GetBytes(salt + valueToValidate);
+            using (var sha = new SHA256Managed())
+            {
+                byte[] hash = sha.ComputeHash(saltedValue);
+                string enteredValueToValidate = Convert.ToBase64String(hash);
+                return encryptedDbValue.Equals(enteredValueToValidate);
+            }
+        }
+
+        string EncryptData(string valueToEncrypt)
+        {
+            string GenerateSalt()
+            {
+                RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider();
+                byte[] salt = new byte[32];
+                crypto.GetBytes(salt);
+                return Convert.ToBase64String(salt);
+            }
+            string EncryptValue(string strValue)
+            {
+                string saltValue = GenerateSalt();
+                byte[] saltedPassword = Encoding.UTF8.GetBytes(saltValue + strValue);
+                using (SHA256Managed sha = new SHA256Managed())
+                {
+                    byte[] hash = sha.ComputeHash(saltedPassword);
+                    return $"{Convert.ToBase64String(hash)}:{saltValue}";
+                }
+            }
+            return EncryptValue(valueToEncrypt);
+        }
+        public bool DatabaseEmpty()
+        {
+            using (var db = new DatabaseContext())
+            {
+                if (db.users.Count() == 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+
+        }
+    }
+}
